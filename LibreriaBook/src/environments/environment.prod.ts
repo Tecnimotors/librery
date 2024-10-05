0df4cfe3f591ff98fd0ad27d17f4bb258a416e29
alonso <programador@tecnimotors.com>
2024-10-05 13:58:36 -0500

    actualizacion librery

--- LibreriaBook/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-
-  //myapiurlrrhh: 'https://api-testing-rrhh.grupotecnimotors.com/api',
-  //myapiurlrrhh: 'http://localhost:50372/api',
-  mylectura: 'https://api-lectura-python.analyticalpoint.com',
-
-};
